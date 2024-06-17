9d9c16db209ff5e163dc3308cd83b1fc644301b3
Flow <guanjun211@gmail.com>
2024-06-17 18:42:29 +0800

    third test close off eslint

--- app/home/parallax.tsx
+++ app/home/parallax.tsx
@@ -1,36 +1,29 @@
 import '@/app/ui/parallax.css';
+import React from 'react';
+
 export default function Parallax() {
     return (
         <div id="parallax-world-of-ugg">
         <section>
           <div className="title">
-            <h3>
-              One's group
-            </h3>
-            <h1>
-              萬萬沒想到
-            </h1>
+            <h3>One's group</h3>
+            <h1>萬萬沒想到</h1>
           </div>
         </section>
         <section>
           <div className="parallax-one">
-            <h2>
-              Ｏrigin
-            </h2>
+            <h2>Origin</h2>
           </div>
         </section>
         
         <section>
           <div className="block">
             <p>
-              <span className="first-character ny">
-              我
-              </span>
-              們的故事始於對奶昔的熱愛與追求。在台灣，我們發現享受奶昔的體驗通常伴隨著高價格。為了讓更多人能以親民的價格品味到美味的奶昔，我們聚集了一群志同道合的夥伴，共同創建了一間當代新潮的奶昔店。
+              <span className="first-character ny">我
+              </span>們的故事始於對奶昔的熱愛與追求。在台灣，我們發現享受奶昔的體驗通常伴隨著高價格。為了讓更多人能以親民的價格品味到美味的奶昔，我們聚集了一群志同道合的夥伴，共同創建了一間當代新潮的奶昔店。
             </p>
           </div>
         </section>
-       
       </div>
     );
   }
