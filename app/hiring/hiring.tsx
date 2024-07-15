04119300a7fa85485847d277e683372ef9ccb2ac
Flow <guanjun211@gmail.com>
2024-07-15 13:53:37 +0800

    hiring first stage

--- app/hiring/hiring.tsx
+++ app/hiring/hiring.tsx
@@ -11,7 +11,7 @@ export default function Hiring() {
                     <section className="top-container">
                     <header className="showcase">
                         <h1>
-                        我們在尋找夥伴
+                        我們正在尋找夥伴
                         </h1>
                         <p>
                         致敬品牌概念美感與文化美學，結合當代AI技術的新創公司，一個公司含有三個薪水袋，眼光放在超越現狀的故事。
@@ -20,7 +20,7 @@ export default function Hiring() {
                         className="btn"
                         href="#"
                         >
-                        Read More
+                        聯絡我們
                         </a>
                     </header>
                     <div className="top-box top-box-a">
@@ -28,13 +28,13 @@ export default function Hiring() {
                         Membership
                         </h4>
                         <p className="price">
-                        $199/mo
+                        行銷經理
                         </p>
                         <a
                         className="btn"
                         href=""
                         >
-                        Buy Now
+                        應徵聯絡
                         </a>
                     </div>
                     <div className="top-box top-box-b">
@@ -42,55 +42,55 @@ export default function Hiring() {
                         Pro Membership
                         </h4>
                         <p className="price">
-                        $299/mo
+                        行政助理
                         </p>
                         <a
                         className="btn"
                         href=""
                         >
-                        Buy Now
+                        應徵聯絡
                         </a>
                     </div>
                     </section>
                     <section className="boxes">
-                    <div className="box">
+                    <div className="box box-1">
                         <i className="fas fa-chart-pie fa-4x" />
                         <h3>
-                        Analytics
+                        內場服務人員
                         </h3>
                         <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?
                         </p>
                     </div>
-                    <div className="box">
+                    <div className="box box-2">
                         <i className="fas fa-globe fa-4x" />
                         <h3>
-                        Marketing
+                        外場服務人員
                         </h3>
                         <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?
                         </p>
                     </div>
-                    <div className="box">
+                    <div className="box box-3">
                         <i className="fas fa-cog fa-4x" />
                         <h3>
-                        Development
+                        萬萬執行店長
                         </h3>
                         <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?
                         </p>
                     </div>
-                    <div className="box">
+                    {/* <div className="box">
                         <i className="fas fa-users fa-4x" />
                         <h3>
-                        Support
+                        萬萬線上客服
                         </h3>
                         <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?
                         </p>
-                    </div>
+                    </div> */}
                     </section>
-                    <section className="info">
+                    {/* <section className="info">
                     <img
                         alt=""
                         src="https://image.ibb.co/j4Qz8x/pic1.jpg"
@@ -109,50 +109,31 @@ export default function Hiring() {
                         Learn More
                         </a>
                     </div>
-                    </section>
+                    </section> */}
                     <section className="portfolio">
                     <img
                         alt=""
-                        src="https://picsum.photos/200/200"
+                        src="https://images.unsplash.com/photo-1614732414444-096e5f1122d5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     />
                     <img
+                        alt=""
+                        src="https://images.unsplash.com/photo-1614313913007-2b4ae8ce32d6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    />
+                    <img
+                        alt=""
+                        src="https://images.unsplash.com/photo-1614730321146-b6fa6a46bcb4?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    />
+                    {/* <img
                         alt=""
                         src="https://picsum.photos/200/200"
                     />
                     <img
                         alt=""
                         src="https://picsum.photos/200/200"
-                    />
-                    <img
-                        alt=""
-                        src="https://picsum.photos/200/200"
-                    />
-                    <img
-                        alt=""
-                        src="https://picsum.photos/200/200"
-                    />
-                    <img
-                        alt=""
-                        src="https://picsum.photos/200/200"
-                    />
-                    <img
-                        alt=""
-                        src="https://picsum.photos/200/200"
-                    />
-                    <img
-                        alt=""
-                        src="https://picsum.photos/200/200"
-                    />
-                    <img
-                        alt=""
-                        src="https://picsum.photos/200/200"
-                    />
+                    /> */}
+
                     </section>
-                    <footer>
-                    <p>
-                        GridBiz © 2018
-                    </p>
-                    </footer>
+
                 </div>
                 </div>
             {/* <Parallax /> */}
