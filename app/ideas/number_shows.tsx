6b54e02335523479ccf08537bf20b180fcc55e1b
Flow <guanjun211@gmail.com>
2024-07-16 16:33:09 +0800

    上傳避免安裝後出事

---
+++ app/ideas/number_shows.tsx
@@ -0,0 +1,80 @@
+import '@/app/ui/ideas/number_shows.css';
+
+export default function NumberShows() {
+    return (
+        <div>
+            <div>
+                <section className="bg-color">
+                    <div className="container">
+                    <h2>
+                        <sup className="dollar">
+                        $
+                        </sup>
+                        <span
+                        className="statistic-count"
+                        data-end="842"
+                        data-start="0"
+                        >
+                        842
+                        </span>
+                        <sup className="plus">
+                        +
+                        </sup>
+                    </h2>
+                    </div>
+                </section>
+                <section>
+                    <div className="container">
+                    <h2>
+                        <sup className="dollar">
+                        $
+                        </sup>
+                        <span
+                        className="statistic-count"
+                        data-end="39181"
+                        data-start="19590"
+                        >
+                        39,181
+                        </span>
+                        <sup className="plus">
+                        +
+                        </sup>
+                    </h2>
+                    </div>
+                </section>
+                <section className="bg-color">
+                    <div className="container">
+                    <h2>
+                        <span
+                        className="statistic-count"
+                        data-end="90"
+                        data-start="0"
+                        >
+                        90
+                        </span>
+                        <sup>
+                        %
+                        </sup>
+                    </h2>
+                    </div>
+                </section>
+                <section>
+                    <div className="container">
+                    <h2>
+                        <sup className="dollar">
+                        $
+                        </sup>
+                        <span
+                        className="statistic-count"
+                        data-end="1"
+                        data-start="0"
+                        >
+                        1
+                        </span>
+                        m
+                    </h2>
+                    </div>
+                </section>
+                </div>
+        </div>
+)};
--- app/ideas/page.tsx
+++ app/ideas/page.tsx
@@ -5,6 +5,7 @@ import Script from 'next/script';
 import Video from '@/app/origin/video';
 import Header from '@/app/ideas/header';
 import Intro from '@/app/ideas/intro';
+import NumberShows from '@/app/ideas/number_shows';
 import Featured from '@/app/ideas/featured';
 import Footer from '@/app/ideas/footer';
 
@@ -25,22 +26,34 @@ export default function Page() {
           src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"
         />
 
+        {/* <Script 
+            src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js"
+        /> */}
+w
+        <Script 
+            src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"
+        />
+
           {/* <Script
             src="https://unpkg.com/zdog@1.1.3/dist/zdog.dist.min.js"
-          />
-          <Script
-            src="/warp.js"
           /> */}
+          {/* <Script 
+            src="/waypoints.js"
+          /> */}
+          
+          <Script
+            src="/number_shows.js"
+          />
 
-          {/* <p>Dashboard Page</p> */}
           <div>
             <Header />
             <Video />
             <Intro />
+            <NumberShows />
             <Featured />
             <Footer />
           </div>
-            
+
           </div>
     );
   }
