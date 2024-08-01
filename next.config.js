ed35cccbb80bf874d6cb048148f404b6c3987e55
Flow <guanjun211@gmail.com>
2024-08-01 09:41:55 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    // Other Next.js configuration options
-    transpilePackages: ['three'],
-
-}
-module.exports = nextConfig;
+// next.config.js
+module.exports = {
+    async redirects() {
+      return [];
+    },
+  };
+  
