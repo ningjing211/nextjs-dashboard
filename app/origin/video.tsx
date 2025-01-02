c24d89ed6b895888e377114424ab3e877d265a65
Flow <guanjun211@gmail.com>
2025-01-02 14:27:21 +0800

    make the cover video play on phone

--- app/origin/video.tsx
+++ app/origin/video.tsx
@@ -1,15 +1,24 @@
 export default function Video() {
     return (
-          <div className="w-full h-full"> 
+          <div 
+            className="w-full h-full"
+            data-autoplay="true"
+            data-loop="true"
+            data-video-url="/ideas/everchanging-video-edited.webm"
+            data-wf-ignore="true"
+          > 
             <video
               className="w-full h-full"
               autoPlay
+              data-wf-ignore="true"
               id="video-background"
               loop
               muted
+              playsInline
             >
               <source
                 src="/ideas/everchanging-video-edited.webm"
+                data-wf-ignore="true"
                 type="video/webm"
               />
             </video>
