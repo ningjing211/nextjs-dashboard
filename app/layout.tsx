e2bc72e569e7783b8d13cd65bde99551b28c8d17
Flow <guanjun211@gmail.com>
2025-02-05 16:51:44 +0800

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,38 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 // import SideNav from '@/app/ui/dashboard/sidenav';
 
+import { Metadata } from 'next';
+ 
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | One One',
+    default: 'One One',
+  },
+  description:
+    '每一杯奶昔都是調皮的小精靈，帶著獨特風味和趣味，準備驚喜你味蕾。萬萬不僅僅是一個名字，而是一種生活態度。每個細節都灑滿美學追求，從店內設計到產品包裝，每一吋都展現我們對生活的熱愛。',
+  metadataBase: new URL('https://www.oneone.global'),
+  applicationName: 'One One',
+  openGraph: {
+    type: 'website',
+    siteName: 'www.oneone.global',
+    url: 'https://www.oneone.global/',
+    images: [
+      {
+        url: 'https://www.oneone.global/link-thumbnail.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'zh_TW',
+  },
+  icons: {
+    icon: 'https://www.oneone.global/thumbnail-one-one.png',
+    apple: 'https://www.oneone.global/thumbnail-one-one.png',
+  }
+};
+
+
 export default function RootLayout({
   children,
 }: {
--- public/header.js
+++ public/header.js
@@ -1,11 +1,11 @@
-window.onload = function () {
-    const currentPath = window.location.pathname; // 取得當前網址的路徑
-    const navLinks = document.querySelectorAll(".header-menu-link"); // 選取所有的導航連結
-    navLinks.forEach(link => {
-        if (link.getAttribute("href") === currentPath) {
-            link.parentElement.classList.add("active"); // 將類名加到父元素 <li>
-        }
-    });
-};
+// window.onload = function () {
+//     const currentPath = window.location.pathname; // 取得當前網址的路徑
+//     const navLinks = document.querySelectorAll(".header-menu-link"); // 選取所有的導航連結
+//     navLinks.forEach(link => {
+//         if (link.getAttribute("href") === currentPath) {
+//             link.parentElement.classList.add("active"); // 將類名加到父元素 <li>
+//         }
+//     });
+// };
 
-console.log('loaded header endd')
+// console.log('loaded header endd')
